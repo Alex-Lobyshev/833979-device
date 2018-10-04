d33f20b59165ed34534c0c3763716e13e289848a
Keks <keks@htmlacademy.ru>
2018-10-04 17:43:42 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,25 @@
+var linkMap = document.querySelector("#main__contacts-us-map");
+var formMap = document.querySelector("#modal-window-map");
+var closeMap = document.querySelector("#modal-window-map-close");
+
+linkMap.addEventListener('click', function() {
+	event.preventDefault();
+	formMap.classList.add('modal-window-map-show');
+});
+
+closeMap.addEventListener('click', function() {
+	formMap.classList.remove('modal-window-map-show');
+});
+
+var linkWrite = document.querySelector("#main__contacts-us-button");
+var formWrite = document.querySelector("#modal-window-write");
+var closeWrite = document.querySelector("#modal-window-write-close");
+
+linkWrite.addEventListener('click', function() {
+	event.preventDefault();
+	formWrite.classList.add("modal-window-write-show");
+})
+
+closeWrite.addEventListener('click', function() {
+	formWrite.classList.remove('modal-window-write-show');
+})
